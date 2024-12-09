0695b3e022797d3bedf89ea1e65e0dd2e625f760
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2024-12-09 17:52:19 +0100

    Add support for cancellation token

--- TtssClient/TtssApi.cs
+++ TtssClient/TtssApi.cs
@@ -21,62 +21,76 @@ public record TtssApi
     /// </summary>
     public required string Language { get; init; }
 
-    private static async Task<string> GetStringAsync(Uri uri)
+    private static async Task<string> GetStringAsync(Uri uri, CancellationToken cancellationToken)
     {
         HttpClient client = new();
         Console.WriteLine(uri);
-        var content = await client.GetAsync(uri);
+        var content = await client.GetAsync(uri, cancellationToken);
         if (content.StatusCode is not HttpStatusCode.OK)
             throw new HttpRequestException($"Unable to get from {uri}: {content.StatusCode}");
-        var text = await content.Content.ReadAsStringAsync();
+        var text = await content.Content.ReadAsStringAsync(cancellationToken);
         return text;
     }
 
-    private static async Task<T> GetAsync<T>(Uri uri, JsonTypeInfo<T> jsonTypeInfo)
+    private static async Task<T> GetAsync<T>(Uri uri, JsonTypeInfo<T> jsonTypeInfo, CancellationToken cancellationToken)
     {
-        var text = await GetStringAsync(uri);
+        var text = await GetStringAsync(uri, cancellationToken);
         var response = JsonSerializer.Deserialize(text, jsonTypeInfo);
         if (response is null) throw new NullReferenceException($"response is null for text `{text}`.");
         return response;
     }
 
-    private async Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
+    private async Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request,
+        CancellationToken cancellationToken)
         where TRequest : IRequest where TResponse : IResponse<TResponse> =>
-        await GetAsync(request.ToUri(R4Uri.Create(BaseUri)) & (language => Language), TResponse.TypeInfo);
+        await GetAsync(request.ToUri(R4Uri.Create(BaseUri)) & (language => Language), TResponse.TypeInfo,
+            cancellationToken);
 
-    public async Task<StopsResponse> GetStopsAsync(StopsRequest request) =>
-        await RequestAsync<StopsRequest, StopsResponse>(request);
-    
-    public async Task<StopPointsResponse> GetStopPointsAsync(StopPointsRequest request) =>
-        await RequestAsync<StopPointsRequest, StopPointsResponse>(request);
+    public async Task<StopsResponse>
+        GetStopsAsync(StopsRequest request, CancellationToken cancellationToken = default) =>
+        await RequestAsync<StopsRequest, StopsResponse>(request, cancellationToken);
 
-    public async Task<StopPassagesResponse> GetStopPassagesAsync(StopPassagesRequest request) =>
-        await RequestAsync<StopPassagesRequest, StopPassagesResponse>(request);
-    
-    public async Task<StopPassagesResponse> GetStopPointPassagesAsync(StopPointPassagesRequest request) =>
-        await RequestAsync<StopPointPassagesRequest, StopPassagesResponse>(request);
+    public async Task<StopPointsResponse> GetStopPointsAsync(StopPointsRequest request,
+        CancellationToken cancellationToken = default) =>
+        await RequestAsync<StopPointsRequest, StopPointsResponse>(request, cancellationToken);
+
+    public async Task<StopPassagesResponse> GetStopPassagesAsync(StopPassagesRequest request,
+        CancellationToken cancellationToken = default) =>
+        await RequestAsync<StopPassagesRequest, StopPassagesResponse>(request, cancellationToken);
+
+    public async Task<StopPassagesResponse> GetStopPointPassagesAsync(StopPointPassagesRequest request,
+        CancellationToken cancellationToken = default) =>
+        await RequestAsync<StopPointPassagesRequest, StopPassagesResponse>(request, cancellationToken);
 
     public async Task<SearchStopsResponse>
-        GetAutocompleteStopsJsonAsync(SearchStopsRequest request) =>
-        await RequestAsync<SearchStopsRequest, SearchStopsResponse>(request);
-    
-    public async Task<NearStopsResponse> GetNearStopsAsync(NearStopsRequest request) => await RequestAsync<NearStopsRequest, NearStopsResponse>(request);
+        GetAutocompleteStopsJsonAsync(SearchStopsRequest request, CancellationToken cancellationToken = default) =>
+        await RequestAsync<SearchStopsRequest, SearchStopsResponse>(request, cancellationToken);
 
-    public async Task<TripPassagesResponse> GetTripPassagesAsync(TripPassagesRequest request) =>
-        await RequestAsync<TripPassagesRequest, TripPassagesResponse>(request);
+    public async Task<NearStopsResponse> GetNearStopsAsync(NearStopsRequest request,
+        CancellationToken cancellationToken = default) =>
+        await RequestAsync<NearStopsRequest, NearStopsResponse>(request, cancellationToken);
 
-    public async Task<RoutesResponse> GetRoutesAsync(RoutesRequest request) =>
-        await RequestAsync<RoutesRequest, RoutesResponse>(request);
+    public async Task<TripPassagesResponse> GetTripPassagesAsync(TripPassagesRequest request,
+        CancellationToken cancellationToken = default) =>
+        await RequestAsync<TripPassagesRequest, TripPassagesResponse>(request, cancellationToken);
 
-    public async Task<RouteStopsResponse> GetRouteStopsAsync(RouteStopsRequest request) =>
-        await RequestAsync<RouteStopsRequest, RouteStopsResponse>(request);
+    public async Task<RoutesResponse> GetRoutesAsync(RoutesRequest request,
+        CancellationToken cancellationToken = default) =>
+        await RequestAsync<RoutesRequest, RoutesResponse>(request, cancellationToken);
 
-    public async Task<PathsResponse> GetRoutePathsAsync(RoutePathsRequest request) =>
-        await RequestAsync<RoutePathsRequest, PathsResponse>(request);
+    public async Task<RouteStopsResponse> GetRouteStopsAsync(RouteStopsRequest request,
+        CancellationToken cancellationToken = default) =>
+        await RequestAsync<RouteStopsRequest, RouteStopsResponse>(request, cancellationToken);
 
-    public async Task<PathsResponse> GetVehiclePathsAsync(VehiclePathsRequest request) =>
-        await RequestAsync<VehiclePathsRequest, PathsResponse>(request);
+    public async Task<PathsResponse> GetRoutePathsAsync(RoutePathsRequest request,
+        CancellationToken cancellationToken = default) =>
+        await RequestAsync<RoutePathsRequest, PathsResponse>(request, cancellationToken);
 
-    public async Task<VehicleResponse> GetVehiclesAsync(VehicleRequest request) =>
-        await RequestAsync<VehicleRequest, VehicleResponse>(request);
+    public async Task<PathsResponse> GetVehiclePathsAsync(VehiclePathsRequest request,
+        CancellationToken cancellationToken = default) =>
+        await RequestAsync<VehiclePathsRequest, PathsResponse>(request, cancellationToken);
+
+    public async Task<VehicleResponse> GetVehiclesAsync(VehicleRequest request,
+        CancellationToken cancellationToken = default) =>
+        await RequestAsync<VehicleRequest, VehicleResponse>(request, cancellationToken);
 }
